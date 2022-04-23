f76842a0006cf040dbe4e3f7cab4091fde6c1513
CoDan123 <let2350@gmail.com>
2022-04-22 20:25:28 -0600

    Topbar component updates

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,11 +1,28 @@
 import "./topbar.css";
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function Topbar() {
     return (
         <div className="topbar-container">
-            <div className="topbar-left"></div>
-            <div className="topbar-center"></div>
-            <div className="topbar-right"></div>
+
+            <div className="topbar-left">
+                <span className="logo">Social-Med</span>
+            </div>
+
+            <div className="topbar-center">
+                <div className="search-bar">
+                    <SearchIcon />
+                    <input placeholder="Search for friends, posts or videos" className="search-input" />
+                </div>
+            </div>
+
+            <div className="topbar-right">
+                <div className="topbar-links">
+                    <span className="topbar-link">Homepage</span>
+                    <span className="topbar-link">Timeline</span>
+                </div>
+            </div>
+
         </div>
     )
 }
