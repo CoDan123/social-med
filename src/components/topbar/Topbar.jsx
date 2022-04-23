a8a2c1ca1f4048a4db05deab46b0e96b063a2d1d
CoDan123 <let2350@gmail.com>
2022-04-22 19:29:20 -0600

    Added Topbar component

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,11 @@
+import "./topbar.css";
+
+export default function Topbar() {
+    return (
+        <div className="topbar-container">
+            <div className="topbar-left"></div>
+            <div className="topbar-center"></div>
+            <div className="topbar-right"></div>
+        </div>
+    )
+}
