93dd2d49336be11710a22b153438161d4c495bb7
CoDan123 <let2350@gmail.com>
2022-04-27 21:35:45 -0600

    Completed unique component content

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,13 @@
 import './sidebar.css';
 import RssFeedIcon from '@mui/icons-material/RssFeed';
+import ChatIcon from '@mui/icons-material/Chat';
+import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
+import GroupIcon from '@mui/icons-material/Group';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import EventIcon from '@mui/icons-material/Event';
+import SchoolIcon from '@mui/icons-material/School';
 
 export default function Sidebar() {
     return(
@@ -8,39 +16,39 @@ export default function Sidebar() {
                 <ul className="sidebar-list">
                     <li className="sidebar-list-item">
                         <RssFeedIcon className='sidebar-icon'/>
-                        <span className="sidebar-list-item"></span>
+                        <span className="sidebar-list-item">Feed</span>
                     </li>
                     <li className="sidebar-list-item">
-                        <RssFeedIcon className='sidebar-icon'/>
-                        <span className="sidebar-list-item"></span>
+                        <ChatIcon className='sidebar-icon'/>
+                        <span className="sidebar-list-item">Chat</span>
                     </li>
                     <li className="sidebar-list-item">
-                        <RssFeedIcon className='sidebar-icon'/>
-                        <span className="sidebar-list-item"></span>
+                        <PlayCircleFilledWhiteIcon className='sidebar-icon'/>
+                        <span className="sidebar-list-item">Videos</span>
                     </li>
                     <li className="sidebar-list-item">
-                        <RssFeedIcon className='sidebar-icon'/>
-                        <span className="sidebar-list-item"></span>
+                        <GroupIcon className='sidebar-icon'/>
+                        <span className="sidebar-list-item">Groups</span>
                     </li>
                     <li className="sidebar-list-item">
-                        <RssFeedIcon className='sidebar-icon'/>
-                        <span className="sidebar-list-item"></span>
+                        <BookmarkIcon className='sidebar-icon'/>
+                        <span className="sidebar-list-item">Bookmarks</span>
                     </li>
                     <li className="sidebar-list-item">
-                        <RssFeedIcon className='sidebar-icon'/>
-                        <span className="sidebar-list-item"></span>
+                        <HelpOutlineIcon className='sidebar-icon'/>
+                        <span className="sidebar-list-item">Help</span>
                     </li>
                     <li className="sidebar-list-item">
-                        <RssFeedIcon className='sidebar-icon'/>
-                        <span className="sidebar-list-item"></span>
+                        <WorkOutlineIcon className='sidebar-icon'/>
+                        <span className="sidebar-list-item">Jobs</span>
                     </li>
                     <li className="sidebar-list-item">
-                        <RssFeedIcon className='sidebar-icon'/>
-                        <span className="sidebar-list-item"></span>
+                        <EventIcon className='sidebar-icon'/>
+                        <span className="sidebar-list-item">Events</span>
                     </li>
                     <li className="sidebar-list-item">
-                        <RssFeedIcon className='sidebar-icon'/>
-                        <span className="sidebar-list-item"></span>
+                        <SchoolIcon className='sidebar-icon'/>
+                        <span className="sidebar-list-item">Courses</span>
                     </li>
                 </ul>
             </div>
