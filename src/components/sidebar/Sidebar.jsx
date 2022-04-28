1fb64e15824f58c400adedcf7922f847158c0afc
CoDan123 <let2350@gmail.com>
2022-04-27 21:24:41 -0600

    Added sidebar list components

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,48 @@
-import './sidebar.css'
+import './sidebar.css';
+import RssFeedIcon from '@mui/icons-material/RssFeed';
 
 export default function Sidebar() {
     return(
         <div className='sidebar'>
             <div className="sidebar-wrapper">
-
+                <ul className="sidebar-list">
+                    <li className="sidebar-list-item">
+                        <RssFeedIcon className='sidebar-icon'/>
+                        <span className="sidebar-list-item"></span>
+                    </li>
+                    <li className="sidebar-list-item">
+                        <RssFeedIcon className='sidebar-icon'/>
+                        <span className="sidebar-list-item"></span>
+                    </li>
+                    <li className="sidebar-list-item">
+                        <RssFeedIcon className='sidebar-icon'/>
+                        <span className="sidebar-list-item"></span>
+                    </li>
+                    <li className="sidebar-list-item">
+                        <RssFeedIcon className='sidebar-icon'/>
+                        <span className="sidebar-list-item"></span>
+                    </li>
+                    <li className="sidebar-list-item">
+                        <RssFeedIcon className='sidebar-icon'/>
+                        <span className="sidebar-list-item"></span>
+                    </li>
+                    <li className="sidebar-list-item">
+                        <RssFeedIcon className='sidebar-icon'/>
+                        <span className="sidebar-list-item"></span>
+                    </li>
+                    <li className="sidebar-list-item">
+                        <RssFeedIcon className='sidebar-icon'/>
+                        <span className="sidebar-list-item"></span>
+                    </li>
+                    <li className="sidebar-list-item">
+                        <RssFeedIcon className='sidebar-icon'/>
+                        <span className="sidebar-list-item"></span>
+                    </li>
+                    <li className="sidebar-list-item">
+                        <RssFeedIcon className='sidebar-icon'/>
+                        <span className="sidebar-list-item"></span>
+                    </li>
+                </ul>
             </div>
         </div>
     )
