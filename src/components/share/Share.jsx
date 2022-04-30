ae3297bbece6578ebca4afc31750fb65422279c8
CoDan123 <let2350@gmail.com>
2022-04-29 21:40:04 -0600

    Share section

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -4,7 +4,10 @@ export default function Share() {
     return (
         <div className="share">
             <div className="share-wrapper">
-                
+                <div className="share-top">
+                    <img className="share-profile-img" src="" alt="" />
+                </div>
+                <div className="share-bottom"></div>
             </div>
         </div>
     )
