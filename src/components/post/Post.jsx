262d5abb9ad6a1db88a19301b6bc8470e34e2059
CoDan123 <let2350@gmail.com>
2022-05-07 21:23:10 -0600

    Post component render in feed

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -2,8 +2,12 @@ import "./post.css";
 
 export default function Post() {
     return (
-        <div>
-
+        <div className="post">
+            <div className="post-wrapper">
+                <div className="post-top"></div>
+                <div className="post-center"></div>
+                <div className="post-bottom"></div>
+            </div>
         </div>
     )
 }
