455063a6877064d689cf540b7de56691e77687ea
CoDan123 <let2350@gmail.com>
2022-05-07 22:12:02 -0600

    Post content

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -4,7 +4,16 @@ export default function Post() {
     return (
         <div className="post">
             <div className="post-wrapper">
-                <div className="post-top"></div>
+                <div className="post-top">
+                    <div className="post-top-left">
+                        <img className="post-profile-img" src="" alt="" />
+                        <span className="post-username">Username here</span>
+                        <span className="post-date">10 mins ago</span>
+                    </div>
+                    <div className="post-top-right">
+
+                    </div>
+                </div>
                 <div className="post-center"></div>
                 <div className="post-bottom"></div>
             </div>
