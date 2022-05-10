a071afadd7d70d26d80edbf1a94d1c361fd882aa
CoDan123 <let2350@gmail.com>
2022-05-09 20:03:36 -0600

    Rightbar content update

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,12 @@ import "./rightbar.css";
 export default function Rightbar() {
     return(
         <div className="right-bar">
-            rightbar
+            <div className="right-bar-wrapper">
+                <div className="birthday-container">
+                    <img className="birthday-img" src="" alt="" />
+                    <span className="birthday-text">John and 7 other friends have a birthday today</span>
+                </div>
+            </div>
         </div>
     )
 }
