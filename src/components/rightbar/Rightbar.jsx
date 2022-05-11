d368bdd144cfac16cebe7c3b72eb88cbe2956694
CoDan123 <let2350@gmail.com>
2022-05-10 21:10:02 -0600

    Added rightbar friends list content

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -9,6 +9,15 @@ export default function Rightbar() {
                     <span className="birthday-text">John and 7 other friends have a birthday today</span>
                 </div>
                 <img className="rightbar-ad" src="" alt="" />
+                <h4 className="rightbar-title">Online Friends</h4>
+                <ul className="rightbar-friend-list">
+                    <li className="rightbar-friend">
+                        <div className="rightbar-profile-img-container">
+                            <img className="rightbar-profile-img" src="" alt="" />
+                            <span className="rightbar-online"></span>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </div>
     )
