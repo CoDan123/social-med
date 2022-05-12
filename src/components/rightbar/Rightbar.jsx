fd54cd1b9877ce884d5731bbd7522d395c474b1a
CoDan123 <let2350@gmail.com>
2022-05-11 22:22:06 -0600

    Rightbar updates

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -18,6 +18,41 @@ export default function Rightbar() {
                         </div>
                         <span className="rightbar-username">John D.</span>
                     </li>
+                    <li className="rightbar-friend">
+                        <div className="rightbar-profile-img-container">
+                            <img className="rightbar-profile-img" src="" alt="" />
+                            <span className="rightbar-online"></span>
+                        </div>
+                        <span className="rightbar-username">John D.</span>
+                    </li>
+                    <li className="rightbar-friend">
+                        <div className="rightbar-profile-img-container">
+                            <img className="rightbar-profile-img" src="" alt="" />
+                            <span className="rightbar-online"></span>
+                        </div>
+                        <span className="rightbar-username">John D.</span>
+                    </li>
+                    <li className="rightbar-friend">
+                        <div className="rightbar-profile-img-container">
+                            <img className="rightbar-profile-img" src="" alt="" />
+                            <span className="rightbar-online"></span>
+                        </div>
+                        <span className="rightbar-username">John D.</span>
+                    </li>
+                    <li className="rightbar-friend">
+                        <div className="rightbar-profile-img-container">
+                            <img className="rightbar-profile-img" src="" alt="" />
+                            <span className="rightbar-online"></span>
+                        </div>
+                        <span className="rightbar-username">John D.</span>
+                    </li>
+                    <li className="rightbar-friend">
+                        <div className="rightbar-profile-img-container">
+                            <img className="rightbar-profile-img" src="" alt="" />
+                            <span className="rightbar-online"></span>
+                        </div>
+                        <span className="rightbar-username">John D.</span>
+                    </li>
                 </ul>
             </div>
         </div>
