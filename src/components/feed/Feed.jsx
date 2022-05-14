d1ea670a0fad25e17fa1976d2ac172f0a9f91ee3
CoDan123 <let2350@gmail.com>
2022-05-13 20:24:33 -0600

    Post component rendered in feed

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,14 +1,17 @@
 import "./feed.css";
 import Share from "../share/Share";
 import Post from "../post/Post";
-import { someData} from "../../someData";
+import { Posts } from "../../someData";
 
 export default function Feed(){
     return(
         <div className="feed">
             <div className="feed-wrapper">
                 <Share />
-                <Post />
+                {Posts.map((p)=>(
+                   <Post key={p.id} post={p}/> 
+                ))}
+                
             </div>
         </div>
     )
