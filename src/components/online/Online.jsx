38bc14525d963aa6bfd7753a4df67f933bfc6445
CoDan123 <let2350@gmail.com>
2022-05-18 21:25:16 -0600

    Online component content

---
+++ src/components/online/Online.jsx
@@ -0,0 +1,13 @@
+import "./online.css";
+
+export default function Online(){
+    return (
+        <li className="rightbar-friend">
+            <div className="rightbar-profile-img-container">
+                <img className="rightbar-profile-img" src="" alt="" />
+                <span className="rightbar-online"></span>
+            </div>
+            <span className="rightbar-username">John D.</span>
+        </li>
+    )
+}
