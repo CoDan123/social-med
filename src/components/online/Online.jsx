b07512ddca4f8322d76aeecde8cfa22bd491722d
CoDan123 <let2350@gmail.com>
2022-05-18 21:45:46 -0600

    Online component styling

--- src/components/online/Online.jsx
+++ src/components/online/Online.jsx
@@ -4,10 +4,10 @@ export default function Online({user}){
     return (
         <li className="rightbar-friend">
             <div className="rightbar-profile-img-container">
-                <img className="rightbar-profile-img" src="" alt="" />
+                <img className="rightbar-profile-img" src={user.profilePicture} alt="" />
                 <span className="rightbar-online"></span>
             </div>
-            <span className="rightbar-username">John D.</span>
+            <span className="rightbar-username">{user.username}</span>
         </li>
     )
 }
