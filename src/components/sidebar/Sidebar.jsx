d2af07cad36d50952a265ad7b7dae34dc49a5f0b
CoDan123 <let2350@gmail.com>
2022-05-19 19:48:18 -0600

    Added close friend component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -58,62 +58,7 @@ export default function Sidebar() {
                         <img className='sidebar-friend-img' src="" alt="" />
                         <span className="sidebar-friend-name">Jane Doe</span>
                     </li>
-                    <li className="sidebar-friend">
-                        <img className='sidebar-friend-img' src="" alt="" />
-                        <span className="sidebar-friend-name">Jane Doe</span>
-                    </li>
-                    <li className="sidebar-friend">
-                        <img className='sidebar-friend-img' src="" alt="" />
-                        <span className="sidebar-friend-name">Jane Doe</span>
-                    </li>
-                    <li className="sidebar-friend">
-                        <img className='sidebar-friend-img' src="" alt="" />
-                        <span className="sidebar-friend-name">Jane Doe</span>
-                    </li>
-                    <li className="sidebar-friend">
-                        <img className='sidebar-friend-img' src="" alt="" />
-                        <span className="sidebar-friend-name">Jane Doe</span>
-                    </li>
-                    <li className="sidebar-friend">
-                        <img className='sidebar-friend-img' src="" alt="" />
-                        <span className="sidebar-friend-name">Jane Doe</span>
-                    </li>
-                    <li className="sidebar-friend">
-                        <img className='sidebar-friend-img' src="" alt="" />
-                        <span className="sidebar-friend-name">Jane Doe</span>
-                    </li>
-                    <li className="sidebar-friend">
-                        <img className='sidebar-friend-img' src="" alt="" />
-                        <span className="sidebar-friend-name">Jane Doe</span>
-                    </li>
-                    <li className="sidebar-friend">
-                        <img className='sidebar-friend-img' src="" alt="" />
-                        <span className="sidebar-friend-name">Jane Doe</span>
-                    </li>
-                    <li className="sidebar-friend">
-                        <img className='sidebar-friend-img' src="" alt="" />
-                        <span className="sidebar-friend-name">Jane Doe</span>
-                    </li>
-                    <li className="sidebar-friend">
-                        <img className='sidebar-friend-img' src="" alt="" />
-                        <span className="sidebar-friend-name">Jane Doe</span>
-                    </li>
-                    <li className="sidebar-friend">
-                        <img className='sidebar-friend-img' src="" alt="" />
-                        <span className="sidebar-friend-name">Jane Doe</span>
-                    </li>
-                    <li className="sidebar-friend">
-                        <img className='sidebar-friend-img' src="" alt="" />
-                        <span className="sidebar-friend-name">Jane Doe</span>
-                    </li>
-                    <li className="sidebar-friend">
-                        <img className='sidebar-friend-img' src="" alt="" />
-                        <span className="sidebar-friend-name">Jane Doe</span>
-                    </li>
-                    <li className="sidebar-friend">
-                        <img className='sidebar-friend-img' src="" alt="" />
-                        <span className="sidebar-friend-name">Jane Doe</span>
-                    </li>
+                    
                 </ul>
             </div>
         </div>
