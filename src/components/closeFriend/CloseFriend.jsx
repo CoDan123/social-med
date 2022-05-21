a72e58bf444cd33008474598508c93cf3c6f4a89
CoDan123 <let2350@gmail.com>
2022-05-20 21:02:58 -0600

    CloseFriend component list item added

---
+++ src/components/closeFriend/CloseFriend.jsx
@@ -0,0 +1,10 @@
+import "./closeFriend.css";
+
+export default function CloseFriend(){
+    return (
+        <li className="sidebar-friend">
+            <img className='sidebar-friend-img' src="" alt="" />
+            <span className="sidebar-friend-name">Jane Doe</span>
+        </li>
+    )
+}
