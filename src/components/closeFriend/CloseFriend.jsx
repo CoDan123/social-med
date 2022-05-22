6d8d5472b96cbdc334a8e54739065916ded7287e
CoDan123 <let2350@gmail.com>
2022-05-21 19:40:24 -0600

    Added CloseFriend props

--- src/components/closeFriend/CloseFriend.jsx
+++ src/components/closeFriend/CloseFriend.jsx
@@ -1,9 +1,9 @@
 import "./closeFriend.css";
 
-export default function CloseFriend(){
+export default function CloseFriend({user}){
     return (
         <li className="sidebar-friend">
-            <img className='sidebar-friend-img' src="" alt="" />
+            <img className='sidebar-friend-img' src={user.profilePicture} alt="" />
             <span className="sidebar-friend-name">Jane Doe</span>
         </li>
     )
