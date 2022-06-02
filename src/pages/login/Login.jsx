a9fa4f546f6e456fc5d2faed39750e17af102911
CoDan123 <let2350@gmail.com>
2022-06-02 13:28:02 -0600

    Login component styling

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,8 +2,16 @@ import "./login.css";
 
 export default function Login() {
     return (
-        <div>
-
+        <div className="login">
+            <div className="login-wrapper">
+                <div className="login-left">
+                    <h3 className="login-logo">Social-Med</h3>
+                    <span className="login-desc">
+                        Connect with friends and the world around you with Social-Med
+                    </span>
+                </div>
+                <div className="login-right"></div>
+            </div>
         </div>
     )
 }
