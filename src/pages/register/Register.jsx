0325efdf87bf1921036d0f76f7123ca1b5403b5a
CoDan123 <let2350@gmail.com>
2022-06-02 20:06:15 -0600

    Added register component

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,24 @@
+import "./register.css";
+
+export default function Register() {
+    return (
+        <div className="login">
+            <div className="login-wrapper">
+                <div className="login-left">
+                    <h3 className="login-logo">Social-Med</h3>
+                    <span className="login-desc">
+                        Connect with friends and the world around you with Social-Med
+                    </span>
+                </div>
+                <div className="login-right">
+                    <div className="login-box">
+                        <input placeholder="Email" className="login-input" />
+                        <input placeholder="Password" className="login-input" />
+                        <button className="login-button">Sign Up</button>
+                        <button className="login-register-button">Create a new account</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
